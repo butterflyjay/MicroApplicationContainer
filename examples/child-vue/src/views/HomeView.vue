8ed9b52c107d73bce3797e738ce1558fb057b1da
‭butterflyjay <1570726985@qq.com>
2023-02-25 10:18:42 +0800

    delete child app

--- examples/child-vue/src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>
-    <micro-app name="react" url="http://localhost:3000/"></micro-app>
-  </div>
-</template>
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,8 @@
 /**
+ * 是否为浏览器环境
+ */
+export const isBrowser = typeof window !== "undefined";
+/**
  * 获取静态资源
  * @param {string} entry 静态资源地址
  * @returns
@@ -7,4 +11,19 @@ export function fetchSource(entry: string): Promise<string> {
   return fetch(entry).then(res => {
     return res.text();
   });
+}
+/**
+ * 判断目标是否为字符串
+ * @param target 目标
+ * @returns {boolean}
+ */
+export function isString(target: unknown): target is string {
+  return typeof target === "string";
+}
+export function formatAppEntry(
+  entry: string | null,
+  appName: string | null = null
+): string {
+  if (!isString(entry) || !entry) return "";
+  return "";
 }
