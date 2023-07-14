9b0a0ed5b9ccb7fe5490971d996386c06ebbb8f6
‭butterflyjay <1570726985@qq.com>
2023-07-14 15:04:14 +0800

    feat: utils function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
-import { MicroElement } from "./element";
+import { PROTOTYPE } from "./types/constants";
+
 /**
  * 是否为浏览器环境
  */
@@ -9,7 +10,7 @@ export const isBrowser = typeof window !== "undefined";
  * @returns
  */
 export function fetchSource(entry: string): Promise<string> {
-  return window.fetch(entry).then(res => {
+  return fetch(entry).then(res => {
     return res.text();
   });
 }
@@ -54,6 +55,39 @@ export function isPromise(target: unknown): target is Promise<unknown> {
   return toString.call(target) === "[object Promise]";
 }
 
+export function isBoolean(target: unknown): target is boolean {
+  return typeof target === "boolean";
+}
+/**
+ * 判断目标是否为bind后的函数
+ * @param target 检测目标
+ * @returns boolean
+ */
+export function isBoundFunction(target: unknown): boolean {
+  return (
+    isFunction(target) &&
+    target.name.indexOf("bound") === 0 &&
+    !target.hasOwnProperty("prototype")
+  );
+}
+
+export function isConstructor(target: unknown): boolean {
+  if (isFunction(target)) {
+    const targetStr = target.toString();
+    return (
+      (target.prototype?.constructor === target &&
+        Object.getOwnPropertyNames(target.prototype).length > 1) ||
+      /^function\s+[A-Z]/.test(targetStr) ||
+      /^class\s+/.test(targetStr)
+    );
+  }
+  return false;
+}
+
+export function isProxyDocument(target: unknown): target is Document {
+  return toString.call(target) === "[object ProxyDocument]";
+}
+
 export function formatAppEntry(
   entry: string | null,
   appName: string | null = null
@@ -78,13 +112,13 @@ export function createUrl(entry: string): URL {
   return new URL(entry);
 }
 
-// export function formatEntry(entry: string, el: MicroElement): string {
+// export function formatEntry(entry: string, el: MicroAppElement): string {
 //   if (!isString(entry) || !entry) return "";
 //   try {
 //     //origin: 包含协议名、域名和端口号 pathname：以"/"起头紧跟着URL文件路径 search：指示URL的参数字符串
 //     const { origin, pathname, search } = createUrl(addProtocol(entry));
 //     console.log(origin, pathname, search);
-//     el.url = { origin, pathname, search };
+//     el.appEntry = { origin, pathname, search };
 //     return `${origin}${pathname}${search}`;
 //   } catch (e) {
 //     console.error(e, el.appName);
@@ -92,17 +126,69 @@ export function createUrl(entry: string): URL {
 //   }
 // }
 
-let currentMicroWebName: string | null = null;
-export function setCurrentAppName(appName: string | null): void {
-  currentMicroWebName = appName;
-}
-export function getCurrentAppName(): string | null {
-  return currentMicroWebName;
+export const {
+  defineProperty,
+  defineProperties,
+  entries,
+  assign,
+  keys,
+  getOwnPropertyDescriptor,
+} = Object;
+
+export const {
+  //EventTarget
+  addEventListener,
+  //Node
+  appendChild,
+  insertBefore,
+  replaceChild,
+  //ParentNode,
+  append,
+  prepend,
+  replaceChildren,
+  //ChildNode,
+  after,
+  before,
+  replaceWith,
+} = HTMLElement[PROTOTYPE];
+
+export function addEventListenerTo(
+  target: EventTarget,
+  type: string,
+  listener: EventListenerOrEventListenerObject,
+  captureOrOptions?: AddEventListenerOptions | boolean
+): void {
+  addEventListener.call(target, type, listener, captureOrOptions);
 }
 
-export function CompletionPath(path: string, baseURL: string): string {
-  if (!path || /^((((ht|f)tps?)|file):)?\/\//.test(path) || /^(data|blob):/.test(path)) {
-    return path;
-  }
-  return baseURL + path;
+export function appendChildTo(node: Node, newChild: Node): Node {
+  return appendChild.call(node, newChild);
 }
+
+export function appendTo(node: Node, ...args: Array<Node | string>) {
+  append.apply(node, args);
+}
+
+export function defer(fn: Function, ...args: unknown[]): void {
+  Promise.resolve().then(fn.bind(null, ...args));
+}
+
+export function isUndefined(target: unknown): target is undefined {
+  return typeof target === "undefined";
+}
+
+export function isPath(url: string) {
+  return !(!url || /^((((ht|f)tps?)|file):)?\/\//.test(url) || /^(data|blob):/.test(url));
+}
+
+export function toCamelCase(attrName: string): string {
+  const list = attrName.split("-");
+  let newName = list[0];
+  for (let i = 1, { length } = list; i < length; ++i) {
+    const [firstLetter, ...rest] = list[i];
+    newName += firstLetter.toUpperCase() + rest.join("");
+  }
+  return newName;
+}
+
+export const domParser = new DOMParser();
