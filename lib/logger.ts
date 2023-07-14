1aaad6010e03b3a54a985480f93f30a8370c9fd5
‭butterflyjay <1570726985@qq.com>
2023-07-14 15:11:34 +0800

    feat: messageLog

--- lib/logger.ts
+++ lib/logger.ts
@@ -8,17 +8,17 @@ class Logger {
   }
   public log(msg: unknown, appName?: string): void {
     if (this.isShowLogger) {
-      console.log(`[Micro Web--${appName ? appName : ""}]: `, msg);
+      console.log(`[Micro Frontend${appName ? "--" + appName : ""}]: `, msg);
     }
   }
   public warn(msg: unknown, appName?: string): void {
     if (this.isShowLogger) {
-      console.warn(`[Micro Web--${appName ? appName : ""}]: `, msg);
+      console.warn(`[Micro Frontend${appName ? "--" + appName : ""}]: `, msg);
     }
   }
   public error(msg: unknown, appName?: string): void {
     if (this.isShowLogger) {
-      console.error(`[Micro Web--${appName ? appName : ""}]: `, msg);
+      console.error(`[Micro Frontend${appName ? "--" + appName : ""}]: `, msg);
     }
   }
 }
